dbb15dd263eb1b9a7e82fc8cd77b6f8f9cfa359b
Salih <esaeliha@gmail.com>
2020-01-15 02:59:42 +0100

    Added first Testcase

--- services/userservice/microservice/user_test.go
+++ services/userservice/microservice/user_test.go
@@ -32,11 +32,49 @@ func StartService(microservicename string) {
 
 func TestUser_AddUser(t *testing.T) {
 	user = proto.NewUserService("user", cli)
-	req1 := &proto.UserRequest{
-		Name: "user1",
-	}
-	user.GetUsers(context.TODO(), &proto.Request{})
+	req1 := &proto.UserRequest{Name: "user1"}
+
+	fix()
 	res1, err := user.AddUser(context.TODO(), req1)
+
 	assert.Nil(t, err)
 	assert.True(t, res1.Success)
+}
+
+func TestUser_AddDoubleUser(t *testing.T) {
+	user = proto.NewUserService("user", cli)
+	req1 := &proto.UserRequest{Name: "user2"}
+
+	fix()
+	res1, err1 := user.AddUser(context.TODO(), req1)
+	res2, err2 := user.AddUser(context.TODO(), req1)
+
+	assert.Nil(t, err1)
+	assert.True(t, res1.Success)
+
+	assert.Nil(t, err2)
+	assert.False(t, res2.Success)
+}
+
+func TestUser_AddTripleUser(t *testing.T) {
+	user = proto.NewUserService("user", cli)
+	req1 := &proto.UserRequest{Name: "user3"}
+
+	fix()
+	res1, err1 := user.AddUser(context.TODO(), req1)
+	res2, err2 := user.AddUser(context.TODO(), req1)
+	res3, err3 := user.AddUser(context.TODO(), req1)
+
+	assert.Nil(t, err1)
+	assert.True(t, res1.Success)
+
+	assert.Nil(t, err2)
+	assert.False(t, res2.Success)
+
+	assert.Nil(t, err3)
+	assert.False(t, res3.Success)
+}
+
+func fix() {
+	user.GetUsers(context.TODO(), &proto.Request{})
 }
